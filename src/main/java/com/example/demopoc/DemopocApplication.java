2cc31cd23b89e3ddc0479c91717dee24b9e5034e
Ak1324 <97510308+Ak1324@users.noreply.github.com>
2025-01-27 11:39:29 +0530

    demopoc

---
+++ src/main/java/com/example/demopoc/DemopocApplication.java
@@ -0,0 +1,17 @@
+package com.example.demopoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemopocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemopocApplication.class, args);
+	}
+	 @GetMapping("/")
+	    public String home() {
+	        return "Hello, DevOps!";
+	    }
+}
