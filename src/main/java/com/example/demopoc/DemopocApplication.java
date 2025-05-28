f4e8c21616c2d11e2eab0b993a71af7f881ca4f6
Ak1324 <97510308+Ak1324@users.noreply.github.com>
2025-05-28 18:14:54 +0530

    Update DemopocApplication.java

--- src/main/java/com/example/demopoc/DemopocApplication.java
+++ src/main/java/com/example/demopoc/DemopocApplication.java
@@ -3,15 +3,17 @@ package com.example.demopoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-@RestController
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class DemopocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemopocApplication.class, args);
-	}
-	 @GetMapping("/")
-	    public String home() {
-	        return "Hello, DevOps!";
-	    }
+    public static void main(String[] args) {
+        SpringApplication.run(DemopocApplication.class, args);
+    }
+    @GetMapping("/")
+    public String home() {
+        return "Hello, DevOps!";
+    }
 }
